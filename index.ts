ec07eff76e93ebb72daebb090ba860b3f58ce849
Raúl Sánchez <raulsanchez1024@gmail.com>
2023-11-12 09:57:26 -0700

    Initial commit

---
+++ index.ts
@@ -0,0 +1,1 @@
+export { LoggMonster } from './src/usage/loggmonster';
---
+++ src/usage/loggmonster.ts
@@ -0,0 +1,55 @@
+type Options = {
+  /**
+   * Event name
+   *
+   * For example, "New user signed up"
+   */
+  event: string;
+  /**
+   * One single emoji
+   *
+   * For example, "😻"
+   */
+  emoji?: string;
+  /**
+   * User email or ID
+   *
+   * For example, "raul@logg.monster" or a UUID
+   */
+  user?: string;
+};
+
+export class LoggMonster {
+  private readonly key: string;
+
+  constructor(key: string) {
+    this.key = key;
+  }
+
+  public async track(opts: Options) {
+    const body = JSON.stringify({
+      ...opts,
+    });
+
+    const res = await fetch('api.thumbsup.to/logg/create', {
+      method: 'POST',
+      body,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.key}`,
+      },
+    });
+
+    if (!res.ok) throw new Error('Error tracking event');
+
+    return true;
+  }
+}
+
+// const loggMonster = new LoggMonster('hihih');
+
+// loggMonster.track({
+//   event: 'New user created',
+//   emoji: '🎉',
+//   user: 'raul@logg.monster',
+// });
